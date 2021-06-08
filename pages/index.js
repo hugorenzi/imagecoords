bc339ec1ef571427618698f63e32a30a46b0dbf2
Hugo Renzi <hugo.renzi@agencian1.com.br>
2021-06-08 00:20:41 -0300

    Base app done

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import { useState, useRef } from 'react';
+import Head from 'next/head';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content'; 
+
+const App = () => {
+  const [result, setResult] = useState("");
+  const imageRef = useRef(null);
+  const MySwal = withReactContent(Swal);
+
+  const uploader = (e) => {
+    const imageFile = e.target.files[0];
+
+    const reader = new FileReader();
+    reader.addEventListener("load", (e) => {
+      setResult(e.target.result);
+    });
+
+    reader.readAsDataURL(imageFile);
+  }
+
+  const getMousePos = (e) => {
+    const mousePos = {
+      x: e.clientX - e.target.offsetLeft,
+      y: e.clientY - e.target.offsetTop
+    };
+
+    Swal.fire({
+      title: 'Clicked Coords',
+      text: `x:${mousePos.y}, y:${mousePos.y}`
+    });
+  }
+
+  return (
+    <main className="app">
+      <Head>
+        <title>Image Coords</title>
+      </Head >
+
+      <div className="content">
+        <input
+          id="uploader"
+          type="file"
+          onChange={(e) => uploader(e)}
+        />
+
+        <button
+          className="mainButton primary upper"
+          onClick={() => document.getElementById("uploader").click()}
+        >
+          Upload image
+        </button>
+
+        {result && <img ref={imageRef} src={result} onClick={e => getMousePos(e)} />}
+      </div>
+    </main>
+  );
+}
+
+export default App;
